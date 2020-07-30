89396a207f1597993fb0c60fce87fe4b5692bdfa
Vinícius M. Knob <knob.vinicius@gmail.com>
2020-07-30 18:38:40 -0300

    Aula 1 - Create a ButtonLink component

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
+import ButtonLink from '../ButtonLink';
 
 function Menu() {
     return (
@@ -9,6 +10,9 @@ function Menu() {
             <a href="/">
                 <img className="Logo" src={Logo} alt="AluraFlix logo" />
             </a>
+            <ButtonLink className="ButtonLink" href="/">
+                Novo vídeo
+            </ButtonLink>
         </nav>
     );
 }
